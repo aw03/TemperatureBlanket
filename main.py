5d8d87f2830dcaab9cfc22893cbc2d429bdb1e59
aw03 <awu245@gmail.com>
2024-11-29 00:09:46 -0500

    meteo api calls

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 
+
 def get_posts():
     # Define the API endpoint URL
-    url = 'https://api.open-meteo.com/v1/forecast?latitude=40.7143&longitude=-74.006&hourly=temperature_2m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York'
+    url = 'https://archive-api.open-meteo.com/v1/archive?latitude=40.7143&longitude=-74.006&start_date=2022-01-01&end_date=2022-12-31&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York'
 
     try:
         # Make a GET request to the API endpoint using requests.get()
